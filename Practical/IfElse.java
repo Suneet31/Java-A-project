576b4d3d1f9ff062ed561851f54f98fc84d7e1a0
Suneet31 <107342974+Suneet31@users.noreply.github.com>
2022-06-17 12:32:50 +0530

    Add files via upload

---
+++ Practical/IfElse.java
@@ -0,0 +1,15 @@
+class IfElse
+{
+public static void main(String argd[])
+{
+int x=1;
+int y=20;
+if (x+y>30)
+
+System.out.println("x+y is greater than 30");
+
+else
+
+System.out.println("x+y is lesser than 30");
+}
+}
